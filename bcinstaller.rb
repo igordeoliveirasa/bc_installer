a9e68aba5af7a06bfd2afb28c32ea19c452aa33e
Igor de Oliveira Sá <igor.sa@h78902092900025.midgard.local>
2013-10-18 15:26:34 -0300

    kick-off

---
+++ bcinstaller.rb
@@ -0,0 +1,21 @@
+
+
+class Installer
+
+  def list_java_versions
+    puts "Java 1.4"
+    puts "Java 1.5"
+    puts "Java 1.6"
+    puts "Java 1.7"
+    choosed_version = gets.chomp
+  end
+
+  def 
+
+end
+
+
+
+
+
+
