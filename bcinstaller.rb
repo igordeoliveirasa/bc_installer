40aa6194bdb8961668c3e31c81e76b8cd83b7397
Igor de Oliveira Sá <igor.sa@h78902092900025.midgard.local>
2013-10-18 17:38:31 -0300

    almost

--- bcinstaller.rb
+++ bcinstaller.rb
@@ -1,28 +1,40 @@
 
 
+
+
 class Installer
 
   def list_java_versions
-    # to do
+    ret = []
     puts "Listando java versions..."
+    default_java_dir_paths = ['/Library/Java/JavaVirtualMachines', '/System/Library/Java/JavaVirtualMachines']
+    default_java_dir_paths.each do |dir|
+      Dir.chdir(dir)
+      java_dirs = Dir.glob("*")
+      java_dirs.each do |java_dir|
+        ret.push(/(?<major>\d+).(?<minor>\d+)/.match(java_dir))
+      end
+    end
+    ret
   end
 
-  def print_java_versions
-    list = list_java_versions()
-    puts "1 - Java 1.4"
-    puts "2 - Java 1.5"
-    puts "3 - Java 1.6"
-    puts "4 - Java 1.7"
+  def print_java_versions(java_versions)
+    java_versions.each_with_index do |version, i|
+      puts "#{i+1} - #{version}"
+    end
   end
 
   def choose_java_version
-    print_java_versions()
+    puts "Please, choose a version:"
+    java_versions = list_java_versions()
+    print_java_versions(java_versions)
     choosed_version = gets.chomp
+    java_versions[choosed_version.to_i-1]
   end
 
-  def download_bc
+  def download_bc(version)
     # to do
-    puts "Downloading bcprovider"
+    puts "Downloading bcprovider for java #{version}" 
   end
 
   def install_bc
@@ -34,7 +46,7 @@ class Installer
 
   def run
     version = choose_java_version()
-    download_bc()
+    download_bc(version)
     install_bc()
   end
 
