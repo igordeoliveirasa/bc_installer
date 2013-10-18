e8f2a10a175a288989d4e75034900ec3637337dd
Igor de Oliveira Sá <igor.sa@h78902092900025.midgard.local>
2013-10-18 15:33:59 -0300

    prototype

--- bcinstaller.rb
+++ bcinstaller.rb
@@ -3,15 +3,46 @@
 class Installer
 
   def list_java_versions
-    puts "Java 1.4"
-    puts "Java 1.5"
-    puts "Java 1.6"
-    puts "Java 1.7"
+    # to do
+    puts "Listando java versions..."
+  end
+
+  def print_java_versions
+    list = list_java_versions()
+    puts "1 - Java 1.4"
+    puts "2 - Java 1.5"
+    puts "3 - Java 1.6"
+    puts "4 - Java 1.7"
+  end
+
+  def choose_java_version
+    print_java_versions()
     choosed_version = gets.chomp
   end
 
-  def 
+  def download_bc
+    # to do
+    puts "Downloading bcprovider"
+  end
 
+  def install_bc
+    puts "Decompressing bcprovider"
+    puts "Copying files"
+    puts "Writing at java.security"
+    puts "Done!"
+  end
+
+  def run
+    version = choose_java_version()
+    download_bc()
+    install_bc()
+  end
+
+end
+
+if __FILE__ == $0
+  installer = Installer.new
+  installer.run
 end
 
 
